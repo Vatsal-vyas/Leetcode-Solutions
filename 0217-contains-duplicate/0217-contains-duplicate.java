7d23643727c232af406fc7880f52458685e19c44
Vatsal Vyas <123654924+Vatsal-vyas@users.noreply.github.com>
2025-06-26 21:57:23 +0530

    Time: 15 ms (47.39%), Space: 58.2 MB (62.84%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,12 +1,16 @@
+import java.util.HashSet;
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        boolean ans = false;
-        for(int i =0; i<nums.length - 1; i++){
-           if(nums[i] == nums[i+1]){
-            ans = true;
-           }  
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;  // duplicate found
+            }
+            seen.add(num);
         }
-        return ans;
+
+        return false;
     }
 }
